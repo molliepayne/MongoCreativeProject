c2f16ca15273bb8602f6664c6c331fec440eadbf
Mollie Payne <mollie@byu.net>
2019-11-10 17:52:19 +0000

    changes to styles

---
+++ foothillFarmFlowers/public/assets/js/components.js
@@ -0,0 +1,10 @@
+Vue.component('banner', 
+{ 
+    template: `  
+    <div id="banner">  
+     <div class="parallax">
+<h1>Welcome to Our Farm</h1> 
+  <hr>
+</div>
+
+    </div>` })
--- foothillFarmFlowers/public/javascripts/availscript.js
+++ foothillFarmFlowers/public/javascripts/availscript.js
@@ -9,22 +9,30 @@ var app = new Vue({
     flowers: [],
     color: "",
     loading: true,
-    colors: ['red', 'blush', 'pink', 'black', 'lavendar', 'blue', 'purple', 'yellow', 'orange', 'apricot', 'white', 'cream', 'green'],
+    colors: [],
     months: ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'],
-    varieties: ['Ageratum', 'Basil', 'Celosia', 'China Aster', 'Clarkia', 'Clary Sage', 'Columbine', 'Coneflower', 'Cosmos', 'Dianthus', 'Didiscus', 'Daffodil', 'Foxglove', 'Gladiolus', 'Hyacinth', 'Iris', 'Leucojum', 'Lily', 'Muscari', 'Narcissus', 'Peony', 'Phlox', 'Poppy', 'Queen Anne', 'Rudbeckia', 'Salvia', 'Sabiosa', 'Snapdragon', 'Strawflower', 'Sunflower', 'Tulip', 'Yarrow', 'Zinnia'],
+    varieties: [],
     Color: "Show All",
     ColorDrop: "Show All",
     Month: "Show All", 
     MonthDrop: "Show All",
     Variety: "Show All", 
-    VarietyDrop: "Show All"
+    VarietyDrop: "Show All",
+    flowerName: "",
+    flowerColors: "",
+    imageUrl: "",
+    bloomMonths: "",
+    infoLink: "", 
+    flowerVariety: ""
+    
 
   },
   created() {
     //console.log("Created Color Drop: " + this.ColorDrop);
     this.getflowers();
+    //this.getvarieties();
     this.colors.sort();
-    this.varieties.sort();
+    
 
   },
   computed: {
@@ -51,86 +59,38 @@ var app = new Vue({
       return moment(date).format('MMMM Do YYYY h:mm A');
     },
     async getflowers() {
-     
-      var url = "http://foothillfarmflowers.com:3030/getflowers";
-      console.log(url);
-      try {
-        let response = await axios.get(url);
-        this.flowers = response.data;
-        
-        return true;
-      }
-      catch (error) {
-        console.log(error);
-      }
-       
-    },
-    async getflowersColor(color) {
-      // `this` points to the vm instance
-      //console.log("In Get Flowers Color Color Drop: " + this.ColorDrop + " color: "+ color);
-      if(color==="Show All")
-        color="";
-      if(color!="")
-      {
-        this.Color = color;
-        this.ColorDrop = color;
-      }
-      else
-      {
-        //console.log("Chaning color drop...");
-        this.Color = "Show All";
-        this.ColorDrop = "Show All";
-      }
-      var month = "";
+       var month = "";
       if(this.MonthDrop != "Show All")
         month = this.MonthDrop;
-       var variety = "";
+      var variety = "";
       if(this.VarietyDrop != "Show All")
         variety = this.VarietyDrop;
-      //console.log("get flowers Variet: " + variety);
-      //console.log("Color Drop value: " + this.Color); 
-      var url = "http://foothillfarmflowers.com:3030/getflowers?color=" + color + "&month=" + month+ "&variety=" + variety;
-      console.log(url);
-      try {
-        let response = await axios.get(url);
-        this.flowers = response.data;
-        //console.log(this.flowers);
-         //console.log("end Get Flowers Color Color Drop: " + this.ColorDrop + " color: "+ color);
-        return true;
-      }
-      catch (error) {
-        console.log(error);
-      }
-       
-    },
-     async getflowersMonth(month) {
-      // `this` points to the vm instance
-      //console.log("Color Drop: " + this.ColorDrop);
-     
-      if(month==="Show All")
-        month="";
-      if(month!="")
-        this.Month = month;
-      else
-      {
-        this.Month = "Show All";
-        this.MonthDrop = "Show All";
-      }
-     var color = ""
+      var color = "";
       if(this.ColorDrop != "Show All")
         color = this.ColorDrop;
-         var variety = "";
-      if(this.VarietyDrop != "Show All")
-        variety = this.VarietyDrop;
-      //console.log("get flowers color: " + color);
-      //console.log("Color Drop value: " + this.Color); 
-      var url = "http://foothillfarmflowers.com:3030/getflowers?color=" + color + "&month=" + month+ "&variety=" + variety;
-      console.log(url);
+      this.varieties.sort();
+      var url = "http://foothillfarmflowers.com:8080/getflowers?color=" + color + "&month=" + month+ "&variety=" + variety;
+      //console.log(url);
       try {
         let response = await axios.get(url);
         this.flowers = response.data;
-        //console.log(this.flowers);
-        //console.log("Color Drop: " + this.ColorDrop);
+        //console.log("get varieties");
+        for(var i=0; i<response.data.length; i++)
+        {
+         //populate varieties dropdown based on current data 
+          if(this.varieties.indexOf(response.data[i].variety)<0)
+            this.varieties.push(response.data[i].variety);
+          var curColors = response.data[i].colors.split(', ');
+          for(var j = 0; j<curColors.length; j++)
+          {
+            //console.log(curColors[j]);
+            //console.log(this.colors);
+            if(this.colors.indexOf(curColors[j])<0)
+              this.colors.push(curColors[j]);
+          } 
+        }
+        this.colors.sort();
+        //console.log(this.varieties);
         return true;
       }
       catch (error) {
@@ -138,45 +98,46 @@ var app = new Vue({
       }
        
     },
-     async getflowersVariety(variety) {
-      // `this` points to the vm instance
-      //console.log("In Get Flowers Variety Drop: " + this.VarietyDrop + " color: "+ color);
-      if(variety==="Show All")
-        variety="";
-      if(variety!="")
-      {
-        this.Variety = variety;
-        this.VarietyDrop = variety;
-      }
-      else
-      {
-        //console.log("Chaning color drop...");
-        this.Variety = "Show All";
-        this.VarietyDrop = "Show All";
-      }
-      var month = ""
-      if(this.MonthDrop != "Show All")
-        month = this.MonthDrop;
-     var color = ""
-      if(this.ColorDrop != "Show All")
-        color = this.ColorDrop;
-      //console.log("get flowers color: " + color);
-      //console.log("Color Drop value: " + this.Color); 
-      var url = "http://foothillfarmflowers.com:3030/getflowers?color=" + color + "&month=" + month+ "&variety=" + variety;
-      console.log(url);
-      try {
-        let response = await axios.get(url);
-        this.flowers = response.data;
-        //console.log(this.flowers);
-         //console.log("end Get Flowers Color Color Drop: " + this.ColorDrop + " color: "+ color);
-        return true;
-      }
-      catch (error) {
-        console.log(error);
-      }
+    
+    
+    
+    async addFlower(){
+      var url = "http://foothillfarmflowers.com:8080/getflowers";
+      console.log("adding flower");
+      axios.post(url, {
+        name: this.flowerName,
+        colors: this.flowerColors,
+        imageUrl: this.imageUrl,
+        bloomMonths: this.bloomMonths,
+        infoLink: this.infoLink, 
+        variety: this.flowerVariety
+      })
+      .then(response => {})
+        .catch(e => {
+          console.log(e);
+        });
+        if(this.varieties.indexOf(this.flowerVariety)<0)
+          this.varieties.push(this.flowerVariety);
        
+        var curColors = this.flowerColors.split(', ');
+          for(var j = 0; j<curColors.length; j++)
+          {
+            //console.log(curColors[j]);
+            //console.log(this.colors);
+            if(this.colors.indexOf(curColors[j])<0)
+              this.colors.push(curColors[j]);
+          } 
+      //console.log("added: " + this.flowerVariety);
+      //console.log(this.varieties);
+      this.getflowers();
+      this.flowerName = '';
+      this.flowerColors = '';
+      this.imageUrl = '';
+      this.bloomMonths = '';
+      this.infoLink = '';
+      this.flowerVariety = '';
+      
     },
-
 
 
   }
