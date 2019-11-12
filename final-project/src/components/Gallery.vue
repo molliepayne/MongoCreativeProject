47ec8657d7aeea16b7dd65845ff6d76217f9aae5
Mollie Payne <mollie@byu.net>
2019-11-12 04:37:43 +0000

    added hover to instas

---
+++ final-project/src/components/Gallery.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="gallery">
+    <div class="gallery-panel"
+         v-for="photo in photos"
+         :key="photo.id">
+      <img :src="thumbUrl(photo.filename)">
+    </div>
+  </div>
+</template>
--- final-project/src/views/Home.vue
+++ final-project/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Gallery />
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Gallery from '@/components/Gallery.vue';
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
-}
+    Gallery,
+  },
+};
 </script>
